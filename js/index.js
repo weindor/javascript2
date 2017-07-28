f7458ed6043ff5976f1dbe72b87cd5890b4e38e0
Nazar Kyrychenko <kyrychenko.nazar@gmail.com>
2017-07-28 11:38:05 +0000

    gh-pages

--- js/index.js
+++ js/index.js
@@ -1,27 +1,75 @@
-function calcDiscr(a, b, c) {
-    return (b * b) - (4 * a * c);
+function print(text) {
+    document.write(text + '<br>');
 }
-function solveQuadratic(a, b, c) {
-    let x1, x2;
-    let discr = calcDiscr(a, b, c);
-    if (discr < 0){
-        result = 'No roots'
-        result1 = 'No roots';
-        return result;
-        return result1;
-    }else if(discr > 0)
-    result = x1 = (-b - Math.sqrt(calcDiscr(a, b, c)))/(2*a);
-    result1 = x2 = (-b + Math.sqrt(calcDiscr(a, b, c)))/(2*a);
-    return result;
-    return result1;
-    if (discr === 0){
-        return x1,x2 = (-b) / (2*a);
+
+function printArr(arr) {
+    print('Array size is: ' + arr.length);
+    for (var i = 0; i < arr.length; i++) {
+       print(i + 1 + ' ' + arr[i]); 
+    }    
+}
+
+function printArrV2(arr) {
+    print('Array size is: ' + arr.length);
+    arr.forEach(function(el, i) {
+        print(i + 1 + ' ' + el);
+    });    
+}
+
+function printDict(dict) {
+    for (var key in dict) {
+        print(key + ': ' + dict[key]);
     }
 }
-var a = parseFloat(prompt('Enter A'));
-var b = parseFloat(prompt('Enter B'));
-var c = parseFloat(prompt('Enter C'));
 
-var result = solveQuadratic(a, b, c);
-document.write('x1= ' + result);
-document.write(' x2 = ' + result1);
+
+let title,
+    price,
+    description,
+    image;
+    
+    
+    
+    
+let goodTitles = [];
+
+goodTitles.push('Mercedes GL500');
+goodTitles.push('BMW X6');
+goodTitles.push('Tesla Model X');
+
+goodTitles[3] = 'ZAZ Lanos';
+goodTitles.push('Audi TT');
+
+printArrV2(goodTitles);
+
+
+let prices = [60000, 90000, 150000, 10000, 50000];
+
+printArrV2(prices);
+
+printCarsAndPrices(goodTitles, prices);
+
+function printCarsAndPrices(cars, prices) {
+    for (var i = 0; i < cars.length; i++ ) {
+        print(cars[i] + ': ' + prices[i]);
+    }
+}
+
+let cars = {};
+
+cars['fastest'] = 'Tesla Model X';
+cars['cheapest'] = 'ZAZ Lanos';
+cars['largest'] = 'Mercedes GL500';
+
+print('Fastest car is: ' + cars['fastest']);
+print(cars.length);
+printDict(prices);
+
+let mb = {};
+
+mb.title = 'Mercedes GL500';
+mb.price = 90000;
+mb.description = 'Cool car';
+mb.color = 'Black';
+
+print(mb.description);
